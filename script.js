1dc8a92b5cc9d074e47f1afd7dd6e3fef662166a
Ryan Huang <npn.git@gmail.com>
2016-06-29 22:04:32 -0700

    Update static
    Added a new favicon, a link to the old website, and links to news
    articles.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-    $("#show-arrow").click(function() {
-        $("html, body").animate({scrollTop: $("#ap-stats").offset().top}, 800);
-    });
+$(document).ready(function(){$("#show-arrow").click(function() {
+    $("html, body").animate({scrollTop: $("#ap-stats").offset().top}, 800);
+});
 });
