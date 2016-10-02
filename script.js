725238230a9f0ec44fceea093ff49d89829b547b
Justin Yu <justin.v.yu@gmail.com>
2016-10-01 18:48:14 -0700

    Update static

--- script.js
+++ script.js
@@ -291,10 +291,10 @@ $(document).ready(function(){// Sticky Plugin v1.0.4 for jQuery
   });
 }));
 $(document).ready(function(){
-    $("#navbar").sticky({
-        topSpacing: 0,
-        responsiveBreakpoint: 558
-    });
+    // $("#navbar").sticky({
+    //     topSpacing: 0,
+    //     responsiveBreakpoint: 558
+    // });
 
     $("#navbar a").click(function () {
         $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top}, 800);
