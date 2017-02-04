becf54651e98a59a0b68b68e220542dcc9a7a1e4
Justin Yu <justin.v.yu@gmail.com>
2017-02-04 00:09:54 -0800

    Push live to static

---
+++ live/firebase-config.js
@@ -0,0 +1,7 @@
+var config = {
+    apiKey: "AIzaSyDFJgcafj_ds8mh_kBC5MquJcspwTaqVuE",
+    authDomain: "losaltoshacks-6c887.firebaseapp.com",
+    databaseURL: "https://losaltoshacks-6c887.firebaseio.com",
+    storageBucket: "losaltoshacks-6c887.appspot.com",
+    messagingSenderId: "298119990670"
+};
