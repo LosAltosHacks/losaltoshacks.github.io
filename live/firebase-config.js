4877b136501d20d489d8d3bd1cafd27fffad1b14
Ryan Huang <npn.git@gmail.com>
2017-02-12 16:22:22 -0800

    Remove Live

--- live/firebase-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    apiKey: "AIzaSyDFJgcafj_ds8mh_kBC5MquJcspwTaqVuE",
-    authDomain: "losaltoshacks-6c887.firebaseapp.com",
-    databaseURL: "https://losaltoshacks-6c887.firebaseio.com",
-    storageBucket: "losaltoshacks-6c887.appspot.com",
-    messagingSenderId: "298119990670"
-};
