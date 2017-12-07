65866c85ba090840c2a8f8b6f375719dc74956e2
Darryl Yeo <darrylyeo@gmail.com>
2017-12-07 21:41:27 +0000

    Simplify FAQ accordions; open animation

--- js/main.js
+++ js/main.js
@@ -133,21 +133,3 @@ function throttle(func, time = 200){
 		passive: true
 	})
 }
-
-// Accordions
-{
-	var accordions = document.getElementsByClassName("accordion");
-
-	for(var i = 0; i < accordions.length; i++) {
-	        accordions[i].onclick = function() {
-	        this.classList.toggle("active");
-
-	        var panel = this.nextElementSibling;
-	        if (panel.style.display === "block") {
-	            panel.style.display = "none";
-	        } else {
-	            panel.style.display = "block";
-	        }
-    	}
-	}
-}
